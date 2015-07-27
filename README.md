4b4902c1e453e9dbe2d76caf6e104e254fec567d
Dmitry Bondarchuk <dvbondarchuk@gmail.com>
2015-07-27 12:09:46 +0500

    changes

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # bxSecRm
-Removes empty sections
+Скрипт удаляет пустые разделы в дереве CMS 1c-bitrix.
+
+## Version
+0.0.1
+
+## Usage
+Необходимо заменить данный путь на путь установки 1с-bitrix.
+	$_SERVER["DOCUMENT_ROOT"] = "/home/bitrix/www";
---
+++ bxSecRm.php
@@ -0,0 +1,38 @@
+<?php
+ini_set("display_errors",1);
+error_reporting(E_ALL);
+set_time_limit(0);
+ini_set('mbstring.func_overload', "2");
+ini_set('mbstring.internal_encoding', "UTF-8");
+
+$_SERVER["DOCUMENT_ROOT"] = "/home/bitrix/www";
+$DOCUMENT_ROOT = $_SERVER["DOCUMENT_ROOT"];
+
+define("BX_UTF", true);
+define("NO_KEEP_STATISTIC", true);
+define("NOT_CHECK_PERMISSIONS", true);
+define("BX_BUFFER_USED", true);
+
+require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/modules/main/include/prolog_before.php");
+while (ob_get_level())
+    ob_end_flush();
+
+CModule::IncludeModule("iblock");
+
+$sections_list = CIBlockSection::GetTreeList(
+	array(), 
+    array('IBLOCK_ID' => 4, "NAME" => $name), true
+);
+echo "<pre>";
+while($arResult = $sections_list->GetNext()) {
+	$nav = CIBlockSection::GetNavChain(4, $arResult["ID"]);
+	$nav->ExtractFields("nav_");
+	if($nav_ID == 59 && CIBlockSection::GetSectionElementsCount($arResult["ID"]) == 0) {
+		if(!CIBlockSection::Delete($arResult["ID"])){
+			echo $arResult["NAME"] . " removed" . "\n";
+		}
+	}
+}
+
+require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/modules/main/include/epilog_after.php");
+?>
